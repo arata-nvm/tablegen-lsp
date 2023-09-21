86a62ecda2c3741f75aeef9de7a780f286989440
Arata <arata04962@gmail.com>
2023-09-21 12:09:12 +0900

    feat: use LanguageClient in vscode extension

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -1,26 +1,26 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import * as path from 'path';
 import * as vscode from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let client: LanguageClient;
+
 export function activate(context: vscode.ExtensionContext) {
+	const command = context.asAbsolutePath(
+		path.join('..', 'target', 'debug', 'tablegen-lsp')
+	);
+	const serverOptions: ServerOptions = { command };
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tablegen-lsp" is now active!');
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: [{ scheme: 'file', language: 'tablegen' }],
+	};
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tablegen-lsp.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from tablegen-lsp!');
-	});
-
-	context.subscriptions.push(disposable);
+	client = new LanguageClient('tablegen-lsp', serverOptions, clientOptions);
+	client.start();
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
