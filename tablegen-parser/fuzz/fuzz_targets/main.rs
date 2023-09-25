5a1216c72260a8499779180952d1ed029571aa74
Arata <arata04962@gmail.com>
2023-09-25 23:28:54 +0900

    feat: create tablegen-parser-fuzz crate

---
+++ tablegen-parser/fuzz/fuzz_targets/main.rs
@@ -0,0 +1,9 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    if let Ok(text) = std::str::from_utf8(data) {
+        let _ = tablegen_parser::grammar::parse(text);
+    }
+});
