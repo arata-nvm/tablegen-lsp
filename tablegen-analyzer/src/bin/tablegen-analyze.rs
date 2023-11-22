f115de5f01caba8af78e7f668931579af933705c
Arata <arata04962@gmail.com>
2023-11-23 04:08:52 +0900

    feat: update tablegen-analyze cli

--- tablegen-analyzer/src/bin/tablegen-analyze.rs
+++ tablegen-analyzer/src/bin/tablegen-analyze.rs
@@ -5,25 +5,69 @@ use tablegen_parser::parser::TextSize;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() != 5 {
-        println!("usage: tablegen-analyze [def_loc|ref_loc|hover] <file> <line> <char>");
+    if args.len() != 3 {
+        println!("usage: tablegen-analyze [def|ref|hover] <file>");
         return;
     }
 
     let text = fs::read_to_string(&args[2]).unwrap();
     let doc = Document::parse(DocumentId::new(0), &text);
 
-    let line: usize = args[3].parse().unwrap();
-    let char_: usize = args[4].parse().unwrap();
+    fn convert_pos(pos: TextSize, doc: &Document) -> (usize, usize) {
+        let line = doc.pos_to_line(pos).unwrap();
+        let char_ = pos - doc.line_to_pos(line).unwrap();
+        (line + 1, Into::<usize>::into(char_) + 1)
+    }
 
-    let pos_size = doc.line_to_pos(line).unwrap();
-    let char_size: TextSize = char_.try_into().unwrap();
-    let pos = pos_size + char_size;
 
     match args[1].as_str() {
-        "def_loc" => println!("{:?}", doc.get_definition(pos)),
-        "ref_loc" => println!("{:?}", doc.get_references(pos)),
-        "hover" => println!("{:?}", doc.get_hover(pos)),
+        "def" => {
+            println!("--- definitions ---");
+            for cursor_pos in 0..text.len() {
+                let cursor_pos: TextSize = cursor_pos.try_into().unwrap();
+                let Some(symbol) = doc.symbol_map().get_symbol_at(cursor_pos) else { continue; };
+                let (doc_id, range) = symbol.define_loc();
+                println!(
+                        "{cursor:3?} -> {symbol_start:3?}..{symbol_end:3?}: {symbol_name} @ {symbol_doc:?}",
+                        cursor = convert_pos(cursor_pos, &doc),
+                        symbol_start = convert_pos(range.start(), &doc),
+                        symbol_end = convert_pos(range.end(), &doc), 
+                        symbol_name = symbol.name(),
+                        symbol_doc = doc_id,
+                    );
+            }
+        }
+        "ref" => {
+            println!("--- references ---");
+            for cursor_pos in 0..text.len() {
+                let cursor_pos: TextSize = cursor_pos.try_into().unwrap();
+                let Some(symbol) = doc.symbol_map().get_symbol_at(cursor_pos) else { continue; };
+                let refs = symbol.reference_locs();
+                println!("{cursor:3?} -> {symbol_name} @ {symbol_doc:?}:", 
+                    cursor = convert_pos(cursor_pos, &doc),
+                    symbol_name = symbol.name(),
+                    symbol_doc = symbol.define_loc().0,
+                );
+                for (doc_id, range) in refs {
+                    println!(
+                        "    {ref_start:3?}..{ref_end:3?} @ {doc_id:?}",
+                        ref_start = convert_pos(range.start(), &doc),
+                        ref_end = convert_pos(range.end(), &doc), 
+                    );
+                }
+            }
+        }
+        "hover" => {
+            println!("--- hovers ---");
+            for cursor_pos in 0..text.len() {
+                let cursor_pos: TextSize = cursor_pos.try_into().unwrap();
+                let Some(hover) = doc.get_hover(cursor_pos) else { continue; };
+                println!("{cursor:3?} -> {hover:?}", 
+                    cursor = convert_pos(cursor_pos, &doc),
+                    hover = hover,
+                );
+            }
+        }
         _ => unimplemented!(),
     }
 }
