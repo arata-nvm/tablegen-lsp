d281ff88d7d6d966ec88de0d4cf8de6bcda31856
Arata <arata04962@gmail.com>
2023-12-02 11:43:33 +0900

    feat: update binary location

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -1,3 +1,4 @@
+import { arch, platform } from 'os';
 import * as path from 'path';
 import * as vscode from 'vscode';
 import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
@@ -5,8 +6,40 @@ import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-lan
 let client: LanguageClient;
 
 export function activate(context: vscode.ExtensionContext) {
+	var binary = '';
+	switch (platform()) {
+		case 'darwin':
+			switch (arch()) {
+				case 'arm64':
+					binary = 'tablegen-lsp-master-aarch64-apple-darwin';
+					break;
+				case 'x64':
+					binary = 'tablegen-lsp-master-x86_64-apple-darwin';
+					break;
+			}
+			break;
+		case 'linux':
+			switch (arch()) {
+				case 'arm64':
+					binary = 'tablegen-lsp-master-aarch64-unknown-linux-gnu';
+					break;
+				case 'x64':
+					binary = 'tablegen-lsp-master-x86_64-unknown-linux-gnu';
+					break;
+			}
+			break;
+		case 'win32':
+			binary = 'tablegen-lsp-master-x86_64-pc-windows-msvc';
+			break;
+	}
+
+	if (binary === '') {
+		vscode.window.showInformationMessage('This platform is currently not supported');
+		return;
+	}
+
 	const command = context.asAbsolutePath(
-		path.join('..', 'target', 'debug', 'tablegen-lsp')
+		path.join('binary', binary)
 	);
 	const serverOptions: ServerOptions = { command };
 
