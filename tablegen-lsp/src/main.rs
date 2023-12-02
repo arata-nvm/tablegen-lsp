23cbed26daa0f0da0d41fc8e0874d350a392badf
Arata <arata04962@gmail.com>
2023-12-02 11:41:49 +0900

    feat: support windows

--- tablegen-lsp/src/main.rs
+++ tablegen-lsp/src/main.rs
@@ -1,11 +1,21 @@
 use async_lsp::stdio::{PipeStdin, PipeStdout};
-use tablegen_lsp::server::TableGenLanguageServer;
 use tower::ServiceBuilder;
+
+use tablegen_lsp::server::TableGenLanguageServer;
 
 #[tokio::main]
 async fn main() {
-    let stdin = PipeStdin::lock_tokio().unwrap();
-    let stdout = PipeStdout::lock_tokio().unwrap();
+    #[cfg(unix)]
+    let (stdin, stdout) = (
+        PipeStdin::lock_tokio().unwrap(),
+        PipeStdout::lock_tokio().unwrap(),
+    );
+
+    #[cfg(not(unix))]
+    let (stdin, stdout) = (
+        tokio_util::compat::TokioAsyncReadCompatExt::compat(tokio::io::stdin()),
+        tokio_util::compat::TokioAsyncWriteCompatExt::compat_write(tokio::io::stdout()),
+    );
 
     let (mainloop, _) = async_lsp::MainLoop::new_server(|client| {
         ServiceBuilder::new().service(TableGenLanguageServer::new_router(client))
