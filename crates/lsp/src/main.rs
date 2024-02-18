4eb426d7bdcdd7ac26a00fb987a2ceda75b50376
Arata <arata04962@gmail.com>
2024-02-19 03:17:47 +0900

    feat: support non-unix platform

--- crates/lsp/src/main.rs
+++ crates/lsp/src/main.rs
@@ -4,9 +4,16 @@ use lsp::server::Server;
 
 #[tokio::main]
 async fn main() {
+    #[cfg(unix)]
     let (stdin, stdout) = (
         async_lsp::stdio::PipeStdin::lock_tokio().unwrap(),
         async_lsp::stdio::PipeStdout::lock_tokio().unwrap(),
+    );
+
+    #[cfg(not(unix))]
+    let (stdin, stdout) = (
+        tokio_util::compat::TokioAsyncReadCompatExt::compat(tokio::io::stdin()),
+        tokio_util::compat::TokioAsyncWriteCompatExt::compat_write(tokio::io::stdout()),
     );
 
     let (mainloop, _) = async_lsp::MainLoop::new_server(|client| {
