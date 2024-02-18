11eb49702c5bd2926fc1f475d1a7397c8f05da49
Arata <arata04962@gmail.com>
2024-02-19 03:16:05 +0900

    feat: implement initialized request

--- crates/lsp/src/server.rs
+++ crates/lsp/src/server.rs
@@ -1,5 +1,9 @@
+use std::ops::ControlFlow;
+
+use async_lsp::lsp_types::{request, InitializeParams, InitializeResult};
 use async_lsp::router::Router;
-use async_lsp::ClientSocket;
+use async_lsp::{ClientSocket, LanguageServer, ResponseError};
+use futures::future::{ready, BoxFuture};
 
 pub struct Server {
     client: ClientSocket,
@@ -8,10 +12,24 @@ pub struct Server {
 impl Server {
     pub fn new_router(client: ClientSocket) -> Router<Self> {
         let this = Self::new(client);
-        Router::new(this)
+        let mut router = Router::new(this);
+        router.request::<request::Initialize, _>(Self::initialize);
+        router
     }
 
     fn new(client: ClientSocket) -> Self {
         Self { client }
+    }
+}
+
+impl LanguageServer for Server {
+    type Error = ResponseError;
+    type NotifyResult = ControlFlow<async_lsp::Result<()>>;
+
+    fn initialize(
+        &mut self,
+        _params: InitializeParams,
+    ) -> BoxFuture<'static, Result<InitializeResult, Self::Error>> {
+        Box::pin(ready(Ok(InitializeResult::default())))
     }
 }
