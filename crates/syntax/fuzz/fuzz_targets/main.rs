5cc5c2ca42b8a2c339d195b3e7f21e7c6d63e19f
Arata <arata04962@gmail.com>
2025-01-15 10:42:51 +0900

    fix: build error on fuzz crate

--- crates/syntax/fuzz/fuzz_targets/main.rs
+++ crates/syntax/fuzz/fuzz_targets/main.rs
@@ -4,6 +4,6 @@ use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(text) = std::str::from_utf8(data) {
-        let _ = tablegen_parser::grammar::parse(text);
+        let _ = syntax::parse(text);
     }
 });
