ef4743fe9d38c2bd40d975c3f2cce7ac4be04f64
Arata <arata04962@gmail.com>
2026-06-03 19:36:59 +0900

    chore: use biome instead of eslint, prettier

--- vscode/prettier.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Config} */
-export default {
-    printWidth: 100,
-    tabWidth: 4,
-    useTabs: false,
-    semi: true,
-    singleQuote: false,
-    trailingComma: "all",
-};
